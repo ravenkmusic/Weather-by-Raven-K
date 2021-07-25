967ad9027782a41c3141ea7285c67109a491c08b
ravenkmusic <ravenkbeats@gmail.com>
2021-07-25 02:01:44 -0400

    added API
    for weather conditions, current temperature and high and low temperature.

--- src/index.js
+++ src/index.js
@@ -55,3 +55,26 @@ function currentTime() {
 
 let time = document.querySelector("#time");
 time.innerHTML = currentTime(now);
+
+function showTemp(response) {
+  console.log(response.data);
+  let city = document.querySelector("#currentcity");
+  city.innerHTML = response.data.name;
+  let weatherCondition = document.querySelector("#description");
+  weatherCondition.innerHTML = response.data.weather[0].description.toUpperCase();
+  let temperature = document.querySelector("#currentweather");
+  temperature.innerHTML = Math.round(response.data.main.temp);
+  let highTemp = document.querySelector("#high");
+  highTemp.innerHTML = Math.round(response.data.main.temp_max);
+  let lowTemp = document.querySelector("#low");
+  lowTemp.innerHTML = Math.round(response.data.main.temp_min);
+  let windSpeed = document.querySelector("#windspeed");
+  windSpeed.innerHTML = Math.round(response.data.wind.speed);
+  
+}
+
+let cityName = document.querySelector("#city-input");
+let apiKey = "c789e765c19e78f4b69ede7112f55431";
+let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=Tokyo&appid=${apiKey}&units=imperial`;
+
+axios.get(apiUrl).then(showTemp);

f0151320ef5ec950b0783e722931422ad99eb43c
ravenkmusic <ravenkbeats@gmail.com>
2021-07-25 17:07:08 -0400

    added fahrenheit and celsius links
    created functions for the user to click the links for their preferred unit of measurement of temperature.

--- src/index.js
+++ src/index.js
@@ -68,14 +68,21 @@ function showTemp(response) {
   let weatherCondition = document.querySelector("#description");
   weatherCondition.innerHTML = response.data.weather[0].description;
 
+  celsiusTemp = response.data.main.temp;
+  console.log(celsiusTemp);
+  celsiusHighTemp = response.data.main.temp_max;
+  console.log(celsiusHighTemp);
+  celsiusLowTemp = response.data.main.temp_min;
+  console.log(celsiusLowTemp);
+
   let temperature = document.querySelector("#currentweather");
-  temperature.innerHTML = Math.round(response.data.main.temp);
+  temperature.innerHTML = Math.round(celsiusTemp);
 
   let highTemp = document.querySelector("#high");
-  highTemp.innerHTML = Math.round(response.data.main.temp_max);
+  highTemp.innerHTML = Math.round(celsiusHighTemp);
 
   let lowTemp = document.querySelector("#low");
-  lowTemp.innerHTML = Math.round(response.data.main.temp_min);
+  lowTemp.innerHTML = Math.round(celsiusLowTemp);
 
   let precipitation = document.querySelector("#precipitation");
   precipitation.innerHTML = response.data.weather[0].main;
@@ -93,7 +100,7 @@ function showTemp(response) {
 }
 function search(city) {
   let apiKey = "c789e765c19e78f4b69ede7112f55431";
-  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=imperial`;
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
   axios.get(apiUrl).then(showTemp);
 }
 
@@ -104,7 +111,48 @@ function handleSubmit(event) {
   console.log(cityName.value);
 }
 
-search("Baltimore");
+function displayFahr(event) {
+  event.preventDefault();
+  let currentTemperature = document.querySelector("#currentweather");
+  let fahrTemp = (celsiusTemp * 9) / 5 + 32;
+  currentTemperature.innerHTML = Math.round(fahrTemp);
+  console.log(fahrTemp);
+
+  let lowTemperature = document.querySelector("#low");
+  let lowFahrTemp = (celsiusLowTemp * 9) / 5 + 32;
+  lowTemperature.innerHTML = Math.round(lowFahrTemp);
+  console.log(lowFahrTemp);
+
+  let highTemperature = document.querySelector("#high");
+  let highFahrTemp = (celsiusHighTemp * 9) / 5 + 32;
+  highTemperature.innerHTML = Math.round(highFahrTemp);
+  console.log(highFahrTemp);
+}
+
+function displayCelsius(event) {
+  event.preventDefault();
+  let currentTemperature = document.querySelector("#currentweather");
+  currentTemperature.innerHTML = Math.round(celsiusTemp);
+  console.log(currentTemperature);
+
+  let lowTemperature = document.querySelector("#low");
+  lowTemperature.innerHTML = Math.round(celsiusLowTemp);
+
+  let highTemperature = document.querySelector("#high");
+  highTemperature.innerHTML = Math.round(celsiusHighTemp);
+}
+
+let celsiusTemp = null;
+let celsiusLowTemp = null;
+let celsiusHighTemp = null;
 
 let form = document.querySelector("#search-city");
 form.addEventListener("submit", handleSubmit);
+
+let currentFahr = document.querySelector("#current-fahr");
+currentFahr.addEventListener("click", displayFahr);
+
+let currentCels = document.querySelector("#current-celsius", "#high-low-fahr");
+currentCels.addEventListener("click", displayCelsius);
+
+search("Baltimore");
